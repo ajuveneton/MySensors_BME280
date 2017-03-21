7606e72a456015b3d6425d62e70122c7646fa8fa
Juveneton Arnaud <arnaud.juveneton@volvo.com>
2017-03-21 13:19:18 +0100

    radio function added

--- MySensors_BME280.ino
+++ MySensors_BME280.ino
@@ -36,8 +36,42 @@ www.embeddedadventures.com
 // BME MySensors weather multi-sensor
 // Written originaly by Arnaud Juveneton
 
-#include <BME280_MOD-1022.h>
-#include <Wire.h>
+// Enable debug prints to serial monitor
+#define MY_DEBUG 
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+#include <MyConfig.h> // ???
+#include <MySensors.h> // MySensors library
+#include <SPI.h> // SPI library
+#include <Wire.h> // I2C library
+
+// BME280 libraries and variables
+// Bosch BME280 Embedded Adventures MOD-1022 weather multi-sensor Arduino code
+// Written originally by Embedded Adventures
+// https://github.com/embeddedadventures/BME280
+#include <BME280_MOD-1022.h> // BME280 sensor library
+
+#define BARO_CHILD 0
+#define TEMP_CHILD 1
+#define HUM_CHILD 2
+
+const float ALTITUDE = 290; // Altitude of the sensor. Needed to calculate barometric pressure at sea level
+
+// Sleep time between reads (in ms)
+const unsigned long SLEEP_TIME = 2000; 
+
+float lastPressure = -1;
+float lastTemp = -1;
+float lastHum = -1;
+
+boolean metric;
+
+MyMessage tempMsg(TEMP_CHILD, V_TEMP);
+MyMessage humMsg(HUM_CHILD, V_HUM);
+MyMessage pressureMsg(BARO_CHILD, V_PRESSURE);
 
 void setup() 
 {
@@ -53,6 +87,18 @@ void setup()
   BME280.writeOversamplingHumidity(os1x);
 }
 
+void presentation()
+{
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("BME280 Sensor", "0.1");
+
+  // Register sensors to gw (they will be created as child devices)
+  present(BARO_CHILD, S_BARO);
+  present(TEMP_CHILD, S_TEMP);
+  present(HUM_CHILD, S_HUM);
+
+}
+
 void loop() 
 {
    // example of a forced sample.  After taking the measurement the chip goes back to sleep
@@ -63,14 +109,42 @@ void loop()
    
   // read out the data - must do this before calling the getxxxxx routines
   BME280.readMeasurements();
-  Serial.print("Temp= ");
-  Serial.println(BME280.getTemperature());  // must get temp first
-  Serial.print("Humidity= ");
-  Serial.println(BME280.getHumidity());
-  Serial.print("Pressure= ");
-  Serial.println(BME280.getPressure());
+
+ float temperature = BME280.getTemperatureMostAccurate();                    // must get temp first
+  float humidity = BME280.getHumidityMostAccurate();
+  float pressure_local = BME280.getPressureMostAccurate();                    // Get pressure at current location
+  float pressure = pressure_local/pow((1.0 - ( ALTITUDE / 44330.0 )), 5.255); // Adjust to sea level pressure using user altitude
+      
   Serial.println();
-  
-  delay(2000); 
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(metric ? " °C" : " °F");
+  Serial.print("Humidity = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  Serial.print("Pressure = ");
+  Serial.print(pressure);
+  Serial.println(" hPa"); 
+
+  if (temperature != lastTemp) 
+  {
+    send(tempMsg.set(temperature, 1));
+    lastTemp = temperature;
+  }
+
+
+  if (humidity != lastHum) 
+  {
+    send(humMsg.set(humidity, 1));
+    lastHum = humidity;
+  }
+
+  if (pressure != lastPressure) 
+  {
+    send(pressureMsg.set(pressure, 2));
+    lastPressure = pressure;
+  }
+    
+  delay(SLEEP_TIME); // Sleep time until next measurement
 
 }
